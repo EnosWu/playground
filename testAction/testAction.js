e9afa25ef24e3f1546cfa1a2436f4f14d765d627
Enos <p510132006@gmail.com>
2024-03-21 19:24:16 +0800

    test again

--- testAction/testAction.js
+++ testAction/testAction.js
@@ -1,5 +1,36 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import * as exec from '@actions/exec'
+
+/**
+ * Get all git tags
+ *
+ * @returns {null || String} all tags text
+ */
+export async function getGitTagList () {
+  let list = ''
+  const options = {
+    silent: true,
+    listeners: {
+      stdout: data => {
+        // append command response
+        list += data.toString()
+      },
+      debug: data => {
+        core.debug(data.toString())
+      }
+    }
+  }
+
+  // execute failed will be non-zero value
+  const result = await exec.exec('git tag', null, options)
+
+  if (result !== 0) {
+    list = null
+  }
+
+  return list
+}
 
 async function mainProcess () {
   // obtain data first
@@ -7,6 +38,13 @@ async function mainProcess () {
   const ownerName = core.getInput('owner', {required: true})
 
   core.debug(`enos owner name ${ownerName}`)
+
+  const list = await getGitTagList()
+  if (list) {
+    core.debug('list obtained' + list)
+  } else {
+    core.error('failed')
+  }
 }
 
 // execute point
